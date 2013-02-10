b907150cf8bb06ec42c995cbb7b0a71787a006a7
Derek VerLee <derekverlee@gmail.com>
2013-02-10 02:40:39 -0500

    Unrecognized users should not be allowed to log in.

--- project/views.py
+++ project/views.py
@@ -18,61 +18,67 @@ def rt_home(request):
     except DBAPIError:
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
 
+    message = ""
+
     if 'userid' in request.POST:
-        userid = request.params['userid']
-        headers = remember(request,userid)
-        return HTTPFound(
-            location=request.route_url('home',
-                                       username=userid),
-            headers=headers)
+        user_id = request.params['userid']
+        if DBSession.query(User).filter(User.user_id == user_id).first() is None:
+            message = 'Gah! The user name ' + user_id + ' is not known. =['
+        else:
+            headers = remember(request,user_id)
+            return HTTPFound(
+                location=request.route_url('home',
+                                           username=user_id),
+                headers=headers)
     
-    return {'logged_in': authenticated_userid(request) }
+    return {'logged_in': authenticated_userid(request),
+            'message': message}
 
 @view_config(route_name='user_created', renderer='templates/user_created.pt')
 def rt_user_created(request):
-   params = request.params
-   userid = params['userid']
+    params = request.params
+    user_id = params['userid']
 
-   try:
-       one = DBSession.query(User).filter(User.user_id == 'bob').first()
-   except DBAPIError:
-       return Response(conn_err_msg, content_type='text/plain', status_int=500)
+    try:
+        one = DBSession.query(User).filter(User.user_id == 'bob').first()
+    except DBAPIError:
+        return Response(conn_err_msg, content_type='text/plain', status_int=500)
 
-   user = User(userid)
-   DBSession.add(user)
+    user = User(user_id)
+    DBSession.add(user)
 
-   return {'userid': userid, 'success': True, 'message': ""}
+    return {'userid': user_id, 'success': True, 'message': ""}
 
 @view_config(route_name='create_user', renderer='templates/create_user.pt')
 def rt_create_user(request):
-   return {}
+    return {}
 
 @view_config(route_name='event_type_created', renderer='templates/event_type_created.pt')
 def rt_event_type_created(request):
-   params = request.params
-   description = params['description']
+    params = request.params
+    description = params['description']
 
-   et = EventType(description)
-   session = DBSession()
-   session.add(et)
-   session.flush()
-   event_code = et.eid 
+    et = EventType(description)
+    session = DBSession()
+    session.add(et)
+    session.flush()
+    event_code = et.eid
 
-   return {'description': description, 
-           'success': True, 
-           'message': "", 
-           'event_code': event_code}
+    return {'description': description,
+            'success': True,
+            'message': "",
+            'event_code': event_code}
 
 @view_config(route_name='create_event_type', renderer='templates/create_event_type.pt')
 def rt_create_event_type(request):
-   return {}
+    return {}
 
 @view_config(route_name='event_type', renderer='templates/event_type.pt')
 def rt_event_type(request):
     eid = request.matchdict['eventid']
-    
+
     et = DBSession.query(EventType).get(eid)
-    
+
     user_id = authenticated_userid(request)
     if user_id is not None:
         observer = DBSession.query(User).filter(User.user_id==user_id).first()
@@ -81,7 +87,7 @@ def rt_event_type(request):
 
     count = len(et.occurances)
 
-    return {'event_type_description': et.description, 
+    return {'event_type_description': et.description,
             'event_type_code': et.eid,
             'count':count,
             'logged_in': user_id}
@@ -91,7 +97,7 @@ Pyramid is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
 
 1.  You may need to run the "initialize_project_db" script
-    to initialize your database tables.  Check your virtual 
+    to initialize your database tables.  Check your virtual
     environment's "bin" directory for this script and try to run it.
 
 2.  Your database server may not be running.  Check that the
@@ -101,4 +107,3 @@ might be caused by one of the following things:
 After you fix the problem, please restart the Pyramid application to
 try it again.
 """
-
